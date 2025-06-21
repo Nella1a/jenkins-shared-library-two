07cfee2403664f71f491e2eb63f93be3ca46c1f6
Nella1a <m.allen@gmx.at>
2025-06-21 22:04:29 +0200

    add parameters

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,12 +1,12 @@
 #!/user/bin/env groovy
 
-def call() {
+def call(String imageNme) {
     echo "building the docker image"
     withCredentials([usernamePassword(credentialsId: 'docker-hub-cred', passwordVariable: 'PASS', usernameVariable: 'USER')])
             {
-                sh 'docker build -t kanjamn/demo-app:jma-2.0.0 .'
+                sh "docker build -t $imageName ."
                 sh 'echo $PASS | docker login -u $USER --password-stdin'
-                sh 'docker push kanjamn/demo-app:jma-2.0.0'
+                sh "docker push $imageName"
             }
 
 }
