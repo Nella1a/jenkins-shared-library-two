11a92e121906d72d0fca70cf56e39835191a9ac9
Nella1a <m.allen@gmx.at>
2025-06-21 22:19:43 +0200

    fix typo in param name

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,12 +1,12 @@
 #!/user/bin/env groovy
 
-def call(String imageNme) {
+def call(String imageName) {
     echo "building the docker image"
     withCredentials([usernamePassword(credentialsId: 'docker-hub-cred', passwordVariable: 'PASS', usernameVariable: 'USER')])
             {
-                sh 'docker build -t $imageName .'
+                sh "docker build -t $imageName ."
                 sh 'echo $PASS | docker login -u $USER --password-stdin'
-                sh 'docker push $imageName'
+                sh "docker push $imageName"
             }
 
 }
